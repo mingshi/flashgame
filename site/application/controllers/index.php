81fc04d607ae10f36c342fbaffc2d1815cf0182b
mingshi <fivemingshi@gmail.com>
2013-10-10 11:17:17 +0800

    init site file

---
+++ site/application/controllers/index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @FileName    :   index.php
+ * @QQ          :   224156865
+ * @date        :   2013/10/10 11:16:56
+ * @link
+ * @Auth        :   Mingshi <fivemingshi@gmail.com>
+ */
+
+
+
---
+++ site/www/index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @FileName    :   index.php
+ * @QQ          :   224156865
+ * @date        :   2013/10/10 11:15:47
+ * @link
+ * @Auth        :   Mingshi <fivemingshi@gmail.com>
+ */
+
+
+echo "Opening soon ...";
